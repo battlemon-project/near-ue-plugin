a5ed2bd9a65d7ac6b6b044ffc01d96b84f55686b
SavIlAnd <87686203+SavIlAnd@users.noreply.github.com>
2022-08-22 12:17:34 +0300

    Update NearPlugin.cpp

--- Plugins/NearPlugin/Source/NearPlugin/Private/NearPlugin.cpp
+++ Plugins/NearPlugin/Source/NearPlugin/Private/NearPlugin.cpp
@@ -7,8 +7,8 @@
 
 DEFINE_LOG_CATEGORY(LogNear);
 #define HANDLE_WINDOWS "near_lib.dll"
-#define HANDLE_MAC "near_lib.dylib"
-#define HANDLE_LINUX "near_lib.so"
+#define HANDLE_MAC "libnear_lib.dylib"
+#define HANDLE_LINUX "libnear_lib.so"
 
 #if PLATFORM_WINDOWS
 #define HANDLE_PATH "/Content/third_party/bin/" HANDLE_WINDOWS
